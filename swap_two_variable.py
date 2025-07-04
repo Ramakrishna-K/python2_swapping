b48d41fd4294b79378f55ebd311e3335b7aa8304
Ramakrishna-K <ramakrishnakatravth0@gmail.com>
2025-07-04 17:00:36 +0530

    2nd python file swapping

---
+++ swap_two_variable.py
@@ -0,0 +1,44 @@
+# # incorrect approach to swapping
+
+# x = 100
+# y = 200
+
+# x =y
+# y = x
+
+# print(x)
+# print(y)
+
+
+
+# # correct Approach Using Temporary variable
+
+# x= 100
+# y = 200
+# print("before swapping the value")
+# print("x=",x)
+# print("y=",y)
+
+# temp = x 
+# x = y
+# y = temp
+# print("after swapping the value")
+# print("x =", x)
+# print("y=", y)
+
+# # Pythonic Way to Swap Variables
+# x = 100
+# y =200
+
+# x,y = y,x
+
+# print("x=",x)
+# print("y=",y)
+
+# # Assigning Multiple Values to Multiple Variables
+# x ,y ,z = 20,  "Ram", 2.0
+ 
+# print(x)
+# print(y)
+# print(z)
+# print(x, y,z)
